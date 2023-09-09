1d40f531510e43f4a94bb3747712f757980ca0cc
michaelaboah <michaelaboah@gmail.com>
2023-09-09 14:18:11 -0400

    "removed /.vercel folder"

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.8e7596c2.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.0719bd3d.js","_app/immutable/chunks/singletons.b706f7dd.js"];
-export const stylesheets = [];
-export const fonts = [];
